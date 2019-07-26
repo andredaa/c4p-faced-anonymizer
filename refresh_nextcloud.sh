bea3058336a5a150d616c4abd48eb51e2632d3c3
andredaa <post.for.andre@gmail.com>
2019-07-26 20:08:50 +0200

    scan all files in refresh

--- refresh_nextcloud.sh
+++ refresh_nextcloud.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 # sudo -u www-data /var/www/nextcloud/occ files:scan --path="/c4p/files"
-/var/www/nextcloud/occ files:scan --path="/c4p/files/camera_footage"
+# /var/www/nextcloud/occ files:scan --path="/c4p/files/camera_footage"
+/var/www/nextcloud/occ files:scan --all
